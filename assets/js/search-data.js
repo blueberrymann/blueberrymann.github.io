8710e1f9281caac96c4c8728993f4f9e79817a6c
blueberrymann <156981049+blueberrymann@users.noreply.github.com>
2025-02-08 13:19:43 +0000

    Deploying to gh-pages from @ blueberrymann/blueberrymann.github.io@68e08d13b9af8031c333e98aef51930cd67a3654 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -86,7 +86,40 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "post-子集-amp-20250205每日一题",
+            },{id: "post-bfs",
+      
+        title: "BFS",
+      
+      description: "学习BFS思想和板子",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/BFS/";
+        
+      },
+    },{id: "post-20250208力扣每日一题",
+      
+        title: "20250208力扣每日一题",
+      
+      description: "记录每日一题的解题思路的求解代码",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/0208%E6%AF%8F%E6%97%A5%E4%B8%80%E9%A2%98/";
+        
+      },
+    },{id: "post-20250207力扣每日一题",
+      
+        title: "20250207力扣每日一题",
+      
+      description: "记录每日一题的解题思路的求解代码",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/0207%E6%AF%8F%E6%97%A5%E4%B8%80%E9%A2%98/";
+        
+      },
+    },{id: "post-子集-amp-20250205每日一题",
       
         title: "子集 &amp; 20250205每日一题",
       
