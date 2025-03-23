4584f15ad1c5591504a5950b746e48b6a1758453
blueberrymann <156981049+blueberrymann@users.noreply.github.com>
2025-03-23 15:05:49 +0000

    Deploying to gh-pages from @ blueberrymann/blueberrymann.github.io@db5e5a84b355f8181cb663c208ec98d25df9fdcc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -921,46 +921,26 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-资源解耦的联邦学习-一种新型三方博弈激励机制解读",
-          title: '资源解耦的联邦学习：一种新型三方博弈激励机制解读',
-          description: "论文复盘｜Data Assetization via Resources-Decoupled Federated Learning",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-资源解耦的联邦学习-一种新型三方博弈激励机制解读",
+          title: '资源解耦的联邦学习：一种新型三方博弈激励机制解读',
+          description: "论文复盘｜Data Assetization via Resources-Decoupled Federated Learning",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Essay_1/";
             },},{id: "travel-厦门游记-海风-阳光与美食的完美邂逅",
           title: '厦门游记 - 海风、阳光与美食的完美邂逅',
           description: "五天的厦门之旅，被这座海滨城市的魅力深深吸引，不愧是中国最宜居的旅行目的地之一",
